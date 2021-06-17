8ce98ce71a29694be138988be24d43517812ad8e
djwls12 <qlql12@kyonggi.ac.kr>
2021-06-17 12:49:39 +0900

    init

---
+++ src/main/java/com/godd/mymyhome/MymyhomeApplication.java
@@ -0,0 +1,13 @@
+package com.godd.mymyhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymyhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymyhomeApplication.class, args);
+	}
+
+}
